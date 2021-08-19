92b648c9752b6b7946c1738f3a9dbf1c618d6d24
Waheed Khan <wahid.khan74@gmail.com>
2021-08-19 07:23:48 +0000

    add ssl cirtificate

---
+++ src/main/java/com/dell/webservice/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.dell.webservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "This is a webserver running with ssl";
+	}
+}
---
+++ src/test/java/com/dell/webservice/Phase3SslServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dell.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3SslServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
